8b44f9acc1f2089ce1e9b22675d34b4797a3d168
Bren Steven Javier <bso.javier0125@gmail.com>
2021-08-12 20:16:44 +0800

    Added animation for active indicator

--- js/drop-drown/src/index.js
+++ js/drop-drown/src/index.js
@@ -1,4 +1,10 @@
+import './styles/style.css';
+
 const content = document.getElementById('content');
+
+function insertChildren(parent, children){
+    children.forEach( node => parent.appendChild(node) );
+}
 
 function createContainer(tag, clss) {
     const container = document.createElement(tag);
@@ -7,17 +13,39 @@ function createContainer(tag, clss) {
     return container;
 }
 
-function createTab(id) {
-    const container = document.createElement('ul');
-    container.id = id;
+function createTab(text, id) {
+    const tab = document.createElement('li')
+    tab.id = id;
+    tab.className = "nav-tab";
+    tab.textContent = text;
+
+    tab.onclick = function() {
+        document.getElementsByClassName('nav-tab active')[0].classList.remove('active');
+        console.log(this.offsetLeft);
+        document.getElementById('active-tab-indicator').style.left = `${this.offsetLeft + 20}px`;
+        this.classList.add('active');
+    }
     
-    return container;
+    return tab;
 }
+
+
 
 function createNavBar() {
     const navContainer = createContainer('nav', 'nav-container');
-    const tabContainer = createTab('tab-container');
+    const tabContainer = createContainer('ul', 'tab-container');
+    const indicator = document.createElement('span');
+    indicator.id = "active-tab-indicator";
+
+    const home = createTab("Home", 'tab-home');
+    home.classList.add('active');
+
+    const tab1 = createTab("tab1", 'tab-1');
+    const tab2 = createTab("tab2", 'tab-2');
+    const tab3 = createTab("tab3", 'tab-3');
+    const tab4 = createTab("tab4", 'tab-3');
     
+    insertChildren(tabContainer, [indicator,home, tab1, tab2, tab3, tab4])
     navContainer.appendChild(tabContainer);
     return navContainer;
 }
